3a6b0b6efc436a3fa456c2bc1108200ef099e153
tparaoan <tiffany.paraoan@cloudreach>
2022-03-28 23:02:18 -0500

    ADD - intial project for ec2

---
+++ instances.tf
@@ -0,0 +1,21 @@
+resource "aws_instance" "my_public_server" {
+  count = var.number_of_instances
+  ami                    = data.aws_ami.aws_basic_linux.id
+  instance_type          = var.ec2_type
+  subnet_id              = data.aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.my_public_app_sg.id]
+  key_name               = var.my_keypair
+}
+
+resource "aws_instance" "my_private_server"{
+  count = var.number_of_instances
+  ami                   = data.aws_ami.aws_basic_linux.id
+  instance_type         = var.ec2_type
+  subnet_id             = data.aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.my_private_app_sg.id]
+  key_name              = var.my_keypair
+
+   tags = {
+      Name = "public_server_${count.index + 1}"
+  }
+}
